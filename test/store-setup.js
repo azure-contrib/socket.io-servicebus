f9d81820ce67b76cfa9484431e0126ee2a1d55ac
Chris Tavares <ctavares@microsoft.com>
2013-01-23 17:40:10 -0800

    Initial tests, building out start

---
+++ test/store-setup.js
@@ -0,0 +1,10 @@
+var should = require('should');
+
+var SbStore = require('../lib/sbstore.js');
+
+describe("Store setup", function() {
+  it("should be createable", function() {
+    var store = new SbStore();
+  });
+});
+  
